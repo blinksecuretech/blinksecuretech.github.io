a622e8198f5c2308b469efdbc30fac20744532a6
Mamo YZ <yzmamo@gmail.com>
2020-03-12 22:21:31 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -29,18 +29,26 @@ $(() => {
   $('.nav-item a').on('click',function(e){
     e.preventDefault();
     let target = $(this).attr('href');
-    $('html,body').stop().animate({scrollTop:$('#'+target).offset().top}, 800);
+    $('html,body').stop().animate({scrollTop:$('#'+target).offset().top - 30}, 800);
   });
   let headerTop = $("header").offset().top;
+
+
   $(window).on("scroll", () => {
     let wTop = $(window).scrollTop();
     parseInt(wTop) > parseInt(headerTop)
       ? $("header").addClass("sticky")
       : $("header").removeClass("sticky");
 
-    if (wTop + $(window).innerHeight() * 0.5 == $(".problem").offset().top) {
+    if (wTop + $(window).innerHeight() * 0.5 > $(".problem").offset().top) {
       countTo('targetId',99,0);
     }
+    $.each($('section'),function(){
+      if (wTop + 80 > $(this).offset().top) {
+        $('.nav-link').removeClass('active');
+        $('.nav-link[href="'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
   });
 
   let waypoints = $('.time-line-row').waypoint(function(direction) {
@@ -54,6 +62,8 @@ $(() => {
   }, {
     offset: '60%'
   });
+
+
 
   const countTo = (element,to,delay)=>{
     setTimeout(() => {
